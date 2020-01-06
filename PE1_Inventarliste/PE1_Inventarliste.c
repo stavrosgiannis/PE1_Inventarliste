c23a2cf8be50e3b7b1a30d66eb61125d6cddb649
Stav ros <hackerone1@web.de>
2020-01-06 23:04:06 +0100

    final

--- PE1_Inventarliste/PE1_Inventarliste.c
+++ PE1_Inventarliste/PE1_Inventarliste.c
@@ -1,13 +1,9 @@
-//
-//  stock.c
-//
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /* structure to store all information about items */
-
 struct item_s {
 	// each item has a unique id to clearly identify it
 	unsigned id;
@@ -21,6 +17,7 @@ struct item_s {
 	unsigned quantity;
 };
 
+//Konstanten
 #define MAX_NR_ITEMS 10000
 /* all items in stock */
 struct item_s stock[MAX_NR_ITEMS];
@@ -29,7 +26,6 @@ unsigned stock_size = 0;
 
 /* read at most max_nr items from file "filename" and write them to array items
  * the number of items actually read is returned */
-
 void read_items_from_file(char* filename)
 {
 	// storage to preliminarily store the information of an item
@@ -45,6 +41,7 @@ void read_items_from_file(char* filename)
 		// if file could not be opened (wrong path, not readable, ...)
 		// output a short message and immediately exit
 		char resp;
+		setbuf(stdout, 0);
 		printf("Eingabedatei kann nicht geoeffnet werden.\n");
 		scanf("%c", &resp);
 		exit(EXIT_FAILURE);
@@ -73,45 +70,35 @@ void read_items_from_file(char* filename)
 	fclose(fp);
 }
 
-/* compare function for items which orders them by ID */
-
-int order_items_by_id(const void* i1, const void* i2)
-{
-	// first convert pointers to correct type
-	struct item_s* item1 = (struct item_s*)i1;
-	struct item_s* item2 = (struct item_s*)i2;
-	if (item1->id > item2->id) return 1;
-	else if (item1->id < item2->id) return -1;
-	else return 0;
-}
-
-/* function that checks whether some item is in stock */
-
-unsigned item_in_stock(unsigned item_id) {
+//Funktion zur Überprüfung ob ein Artikel (item_id) vorhanden ist
+//Wenn dies der Fall ist erhält serial_number die entsprechende Position im Array
+unsigned item_in_stock(unsigned item_id, int* serial_id) {
 	for (int i = 0; i < stock_size; i++) {
-		if (item_id == stock[i].id) return 1;
+		if (item_id == stock[i].id) {
+			*serial_id = i;
+			return 1;
+		}
 	}
 	return 0;
 }
 
-// --- part 1
-
+//Aufgabe 1
 void enter_desired_items(unsigned int* id, unsigned int* nr) {
 	setbuf(stdout, 0);
-	printf("\nGeben Sie die ID des Artikels ein: ");
+	printf("\nBitte geben Sie die ID des Artikels ein: ");
 	scanf("%i", id);
 	setbuf(stdout, 0);
-	printf("\nGeben Sie die gewuenschte Anzahl ein: ");
+	printf("\nBitte geben Sie die gewuenschte Anzahl ein: ");
 	scanf("%i", nr);
 }
 
-void remove_items_from_stock(unsigned id, unsigned nr) {
-	if (nr <= stock[id].quantity) {
-		stock[id].quantity = stock[id].quantity - nr;
+void remove_items_from_stock(unsigned nr, int serial_id) {
+	if (nr <= stock[serial_id].quantity) {
+		stock[serial_id].quantity = stock[serial_id].quantity - nr;
 		setbuf(stdout, 0);
 		printf("\nDie Buchung war erfolgreich");
 		setbuf(stdout, 0);
-		printf("\nPosition %s: rack%i/pos %i", stock[id].model, stock[id].rack, stock[id].position);
+		printf("\nPosition %s: rack%i/pos %i", stock[serial_id].model, stock[serial_id].rack, stock[serial_id].position);
 	}
 	else {
 		setbuf(stdout, 0);
@@ -119,16 +106,60 @@ void remove_items_from_stock(unsigned id, unsigned nr) {
 	}
 }
 
-// --- part 2
+//Aufgabe 2
+void add_items_to_stock(unsigned id, unsigned nr, int serial_id) {
+	// storage to preliminarily store the information of an item
+	char manufacturer[16]; char model[16];
+	unsigned rack; unsigned position;
+	if (serial_id != (-1)) {
+		stock[serial_id].quantity = stock[serial_id].quantity + nr;
+		setbuf(stdout, 0);
+		printf("\nDer Artikel wurde erfolgreich dem Bestand hinzugefuegt");
+	}
+	else {
+		stock_size++;
+		setbuf(stdout, 0);
+		printf("Der Artikel wurde nicht gefunden. Neu hinzufuegen:\nHersteller Name: ");
+		scanf("%s", manufacturer);
+		setbuf(stdout, 0);
+		printf("Modell: ");
+		scanf("%s", model);
+		setbuf(stdout, 0);
+		printf("Regal: ");
+		scanf("%i", &rack);
+		setbuf(stdout, 0);
+		printf("Position: ");
+		scanf("%i", &position);
 
-struct item_s enter_new_item(unsigned id);
-void add_items_to_stock(unsigned id, unsigned nr);
+		stock[stock_size - 1].id = id;
+		strcpy(stock[stock_size - 1].manufacturer, manufacturer);
+		strcpy(stock[stock_size - 1].model, model);
+		stock[stock_size - 1].rack = rack;
+		stock[stock_size - 1].position = position;
+		stock[stock_size - 1].quantity = nr;
+	}
+}
 
-// --- part 3 (preliminary solution)
+//Aufgabe 3
+/* compare function for items which orders them by ID */
+int order_items_by_id(const void* i1, const void* i2, int position)
+{
+	// first convert pointers to correct type
+	struct item_s* item1 = (struct item_s*)i1;
+	struct item_s* item2 = (struct item_s*)i2;
+	struct item_s buf;
+	if (item1->id > item2->id) {
+		buf = *item1;
+		stock[position] = *item2;
+		stock[position + 1] = buf;
+	}
+	else return 0;
+}
 
 void print_stock()
 {
-	for (unsigned i = 0; i < stock_size; i++)
+	for (unsigned i = 0; i < stock_size; i++) {
+		setbuf(stdout, 0);
 		printf("%04i %s %s - rack %i/pos %i - #items: %i\n",
 			stock[i].id,
 			stock[i].manufacturer,
@@ -136,10 +167,10 @@ void print_stock()
 			stock[i].rack,
 			stock[i].position,
 			stock[i].quantity);
+	}
 }
 
-// --- part 4
-
+// Aufgabe 4
 void save_items_to_file(char* filename) {
 	FILE* fp = fopen(filename, "w");
 	for (int i = 0; i < stock_size; i++) {
@@ -154,14 +185,12 @@ void save_items_to_file(char* filename) {
 	fclose(fp);
 }
 
-/*
- * main function: entry of point
- */
-
+//Main-Function
 int main()
 {
 	// print program header
 	printf("\n\n");
+	setbuf(stdout, 0);
 	printf("HAUPTMENU\n");
 	printf("---------\n\n");
 
@@ -171,12 +200,19 @@ int main()
 	//
 	do {
 		// read the name to be searched
+		setbuf(stdout, 0);
 		printf("\n\nWas wollen Sie tun?\n");
+		setbuf(stdout, 0);
 		printf("(1) Entnahme von Elementen\n");
+		setbuf(stdout, 0);
 		printf("(2) Hinzufuegen von Elementen\n");
+		setbuf(stdout, 0);
 		printf("(3) aktuellen Lagerbestand speichern\n");
+		setbuf(stdout, 0);
 		printf("(4) Inventarliste ausgeben\n");
+		setbuf(stdout, 0);
 		printf("(0) Programm beenden\n");
+		setbuf(stdout, 0);
 		printf("\tIhre Wahl: ");
 
 		unsigned  choice;
@@ -184,25 +220,47 @@ int main()
 		while (getchar() != '\n');
 
 		// menu items
+		unsigned desired_id, desired_quant;
+		//serial_id = Fortlaufende Nummer -> Position im Array. Hat serial_id den Wert -1 ist der Artikel nicht in der Liste vorhanden.
+		int serial_id;
 		switch (choice)
 		{
+			//Entnahme eines Artikels
 		case 1:
-			// TODO
-			remove_items_from_stock();
+			serial_id = (-1);
+			while (1) {
+				enter_desired_items(&desired_id, &desired_quant);
+				if (item_in_stock(desired_id, &serial_id)) {
+					remove_items_from_stock(desired_quant, serial_id);
+					break;
+				}
+				else {
+					setbuf(stdout, 0);
+					printf("\nDer Artikel wurde nicht gefunden. Bitte Eingabe wiederholen");
+				}
+			}
 			break;
 		case 2:
-			// TODO
-			enter_desired_items();
+			//Hinzufügen eines Artikels
+			serial_id = (-1);
+			enter_desired_items(&desired_id, &desired_quant);
+			item_in_stock(desired_id, &serial_id);
+			add_items_to_stock(desired_id, desired_quant, serial_id);
 			break;
 		case 3:
 			save_items_to_file("lager.txt");
 			break;
 		case 4:
+			//Sortierung des Inventars nach ID und Ausgabe
+			for (int n = stock_size - 1; n > 0; n--) {
+				for (int i = 0; i < n; i++) order_items_by_id(&stock[i], &stock[i + 1], i);
+			}
 			print_stock();
 			break;
 		case 0:
 			return 0;
 		default:
+			setbuf(stdout, 0);
 			printf("Ungueltige Eingabe.\n");
 		}
 	} while (1);
